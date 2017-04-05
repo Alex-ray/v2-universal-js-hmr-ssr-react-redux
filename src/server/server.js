061e978a58ff68f9e63d49bb9531b4a95da2cd56
Alexander J Ray <alexjamesray@gmail.com>
2017-04-04 22:56:51 -0700

    Sets up babel and express server

---
+++ src/server/server.js
@@ -0,0 +1,36 @@
+import http    from 'http';
+import express from 'express';
+import colors  from 'colors';
+import path    from 'path';
+
+const PROD = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+if (!PROD) {
+  app.use(function(err, req, res, next) {
+    console.error('error : ', err)
+    res.status(err.status || 500);
+  });
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+  console.error('error : ', err.message)
+  res.status(err.status || 500);
+});
+
+const server = http.createServer(app);
+
+server.listen(8080, function() {
+   const address = server.address();
+   console.log(`${'>>>'.cyan} ${'Listening on:'.rainbow} ${'localhost::'.trap.magenta}${`${address.port}`.green}`);
+ });
